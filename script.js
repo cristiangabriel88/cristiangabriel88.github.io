ff7ba7ae7ed04b8befd18e040e608a348526e67e
cristiangabriel88 <cristiconstantinescu88@gmail.com>
2022-05-18 11:23:15 +0300

    tried js

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
+// Tried fade-out
 window.addEventListener("beforeunload", function () {
   document.body.classList.add("animate-out");
 });
+//
+//
+// Var declarations
+
+// let text = document.getElementById("middle-text").innerHTML;
+let git = document.getElementById("git").innerHTML;
+let linkedIn = document.getElementById("linkedIn");
+let stack = document.getElementById("stack");
+let location = document.getElementById("location");
+let mail = document.getElementById("mail");
+
+//
+// function changeText(event) {
+//  text = event.target
+// }
+
+// EventTarget.onmouseover = changeText;
+
+// git.addEventListener("mouseover", (text = "Git"));
+let text = "sdad";
+document.getElementById("middle-text").innerHTML = "dsadasd";
