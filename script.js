aef759e53abdba615364e755b24450b486be82ff
Cristian Gabriel <77484833+cristiangabriel88@users.noreply.github.com>
2024-10-18 23:09:59 +0300

    Refurbishing 2024

--- script.js
+++ script.js
@@ -2,37 +2,16 @@
 window.addEventListener("beforeunload", function () {
   document.body.classList.add("animate-out");
 });
-//
-//
-// Var declarations
-let age = document.getElementById("age");
-let year = document.getElementById("year");
-// let text = document.getElementById("middle-text");
-// let git = document.getElementById("git");
-// let linkedIn = document.getElementById("linkedIn");
-// let stack = document.getElementById("stack");
-// let location = document.getElementById("location");
-let mail = document.getElementById("mail");
 
-// * Get age function for About Me section///
-let date = new Date();
-age.innerHTML = date.getFullYear() - 1988;
-// year.innerHTML = date.getFullYear();
-//**************************************** */
-// function changeText(event) {
-//  text = event.target
-// }
+window.onload = function () {
+  // Check if the element with id "age" exists
+  let ageElement = document.getElementById("age");
+  if (ageElement) {
+    let currentYear = new Date().getFullYear();
+    ageElement.innerHTML = currentYear - 1988;
+  }
+};
 
-// EventTarget.onmouseover = changeText;
-
-// git.addEventListener("mouseover", (text = "Git"));
-// text.innerHTML = "Hey there!";
-// function showText() {
-// document.getElementById("icon-text").style.visibility = "visible";
-// document.getElementById("git").onmouseover = showText;
-// document.getElementById("icon-text").style.visibility = "visible";
-console.log("HEREE!!");
 function copyText() {
-  /* Copy text into clipboard */
   navigator.clipboard.writeText("cristiconstantinescu88@gmail.com");
 }
