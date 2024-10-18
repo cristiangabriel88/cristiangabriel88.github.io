b1cc0f5378ff9862bf2485aca15d186014cfaec4
Cristian Gabriel <77484833+cristiangabriel88@users.noreply.github.com>
2024-10-18 22:15:44 +0300

    Initial commit

--- script.js
+++ script.js
@@ -6,16 +6,18 @@ window.addEventListener("beforeunload", function () {
 //
 // Var declarations
 let age = document.getElementById("age");
-let text = document.getElementById("middle-text");
-let git = document.getElementById("git");
-let linkedIn = document.getElementById("linkedIn");
-let stack = document.getElementById("stack");
+let year = document.getElementById("year");
+// let text = document.getElementById("middle-text");
+// let git = document.getElementById("git");
+// let linkedIn = document.getElementById("linkedIn");
+// let stack = document.getElementById("stack");
 // let location = document.getElementById("location");
 let mail = document.getElementById("mail");
 
 // * Get age function for About Me section///
 let date = new Date();
 age.innerHTML = date.getFullYear() - 1988;
+// year.innerHTML = date.getFullYear();
 //**************************************** */
 // function changeText(event) {
 //  text = event.target
